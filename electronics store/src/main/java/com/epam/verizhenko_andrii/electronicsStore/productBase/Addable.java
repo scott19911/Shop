a994889805d9990377682b8df9e3da1ae45c8b51
Verizhenko Andrii <scott19911@gmail.com>
2022-08-15 13:46:09 +0300

    task_8

---
+++ electronics store/src/main/java/com/epam/verizhenko_andrii/electronicsStore/productBase/Addable.java
@@ -0,0 +1,9 @@
+package com.epam.verizhenko_andrii.electronicsStore.productBase;
+
+import com.epam.verizhenko_andrii.electronicsStore.products.Product;
+
+import java.util.Map;
+
+public interface Addable {
+   Map<Product,Integer> add();
+}
