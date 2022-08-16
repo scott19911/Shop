63d7d0f83dd70787a375cebb3546af2493c6cd95
Verizhenko Andrii <scott19911@gmail.com>
2022-08-16 16:20:59 +0300

    task7 first part

--- electronics store/src/main/java/com/epam/verizhenko_andrii/electronicsStore/productBase/AddProducts.java
+++ electronics store/src/main/java/com/epam/verizhenko_andrii/electronicsStore/productBase/AddProducts.java
@@ -1,6 +1,7 @@
 package com.epam.verizhenko_andrii.electronicsStore.productBase;
 
 import com.epam.verizhenko_andrii.electronicsStore.products.Product;
+import com.epam.verizhenko_andrii.electronicsStore.reflectionInputer.AddAutogenerateRef;
 
 import java.util.Map;
 import java.util.Scanner;
@@ -9,10 +10,23 @@ public class AddProducts {
     public Map<Product,Integer> addProducts(){
         final Addable addProd;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter type of add: console or auto");
+        System.out.println("Enter type of add: c,a,ar");
         String type = sc.next();
-        addProd = type.equalsIgnoreCase("console")? new AddConsoleImpl():
-                type.equalsIgnoreCase("auto")? new AddAutogenerateImpl():null;
+        switch (type){
+            case "C":
+            case "c": addProd = new AddConsoleImpl();
+            break;
+            case "A":
+            case "a": addProd = new AddAutogenerateImpl();
+            break;
+            case "Ar":
+            case "aR":
+            case "AR":
+            case "ar": addProd = new AddAutogenerateRef();
+            break;
+            default: addProd = null;
+        }
+
         if(addProd == null){
             System.out.println("unsupported command");
             return null;
---
+++ electronics store/src/main/java/com/epam/verizhenko_andrii/electronicsStore/reflectionInputer/AddAutogenerateRef.java
@@ -0,0 +1,74 @@
+package com.epam.verizhenko_andrii.electronicsStore.reflectionInputer;
+
+import com.epam.verizhenko_andrii.electronicsStore.productBase.Addable;
+import com.epam.verizhenko_andrii.electronicsStore.products.Mda;
+import com.epam.verizhenko_andrii.electronicsStore.products.Product;
+import com.epam.verizhenko_andrii.electronicsStore.products.Refregerators;
+
+import java.lang.reflect.Field;
+import java.util.*;
+
+public class AddAutogenerateRef implements Addable {
+    private final Map<String, Product> productMap = new HashMap<>();
+    @Override
+    public Map<Product, Integer> add() {
+        Map<Product, Integer> map = new HashMap<>();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("prod - products\nmda - mda\nref - refrigeration");
+        String pName = sc.next();
+        int nProducts = 1;
+        while (nProducts > 0) {
+            map.put(inpProd(pName), new Random().nextInt(Integer.MAX_VALUE));
+            System.out.println("Add more products 0/1");
+            nProducts = sc.nextInt();
+        }
+        return map;
+    }
+
+    public Product inpProd(String pName) {
+        init();
+        Product prod = productMap.get(pName);
+        Class<Product> clazz = (Class<Product>) prod.getClass();
+        while (!clazz.equals(Object.class)) {
+            Field[] fields = clazz.getDeclaredFields();
+            setParam(prod, fields);
+            clazz = (Class<Product>) clazz.getSuperclass();
+        }
+        System.out.println("Enter quantity");
+        return prod;
+
+    }
+
+    private void setParam(Product prod, Field[] fields) {
+
+        for (Field field : fields) {
+            if (field.getAnnotation(Product.Reflectable.class) != null) {
+
+                field.setAccessible(true);
+                try {
+                    field.set(prod, typeField(field.getType().getName()));
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException(e);
+                }
+
+            }
+        }
+    }
+    Object typeField(String st) {
+        if (st.equals("double")) {
+            return new Random().nextInt(Integer.MAX_VALUE);
+        }
+        if (st.equals("int")) {
+            return new Random(Integer.MAX_VALUE);
+        } else {
+            return "Фирма " + new Random().nextInt(Integer.MAX_VALUE);
+        }
+    }
+
+    public void init() {
+        productMap.put("prod", new Product());
+        productMap.put("ref", new Refregerators());
+        productMap.put("mda", new Mda());
+    }
+
+}
