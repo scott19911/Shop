397c5a8a22c40993c39557ab5988cf875fb71faa
Verizhenko Andrii <scott19911@gmail.com>
2022-08-18 16:43:10 +0300

    update

--- electronics store/src/main/java/com/epam/verizhenko_andrii/electronicsStore/inputer/ConsoleInp.java
+++ electronics store/src/main/java/com/epam/verizhenko_andrii/electronicsStore/inputer/ConsoleInp.java
@@ -1,31 +1,34 @@
 package com.epam.verizhenko_andrii.electronicsStore.inputer;
 
-import com.epam.verizhenko_andrii.electronicsStore.products.Mda;
+import com.epam.verizhenko_andrii.electronicsStore.products.MediumDigitalAppliances;
 import com.epam.verizhenko_andrii.electronicsStore.products.Product;
 import com.epam.verizhenko_andrii.electronicsStore.products.Refregerators;
-import com.epam.verizhenko_andrii.electronicsStore.reflectionInputer.Add;
+import com.epam.verizhenko_andrii.electronicsStore.reflectionInputer.ProductService;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
-public class ConsoleInp<T extends Product> implements Add<T> {
+
+public class ConsoleInp<T extends Product> implements ProductService<T> {
     private final Map<String, Product> productMap = new HashMap<>();
     private final Map<String, Inputer<T>> inputerMap = new HashMap<>();
     T obj;
+
     public ConsoleInp(Object obj) {
         init();
-        this.obj =(T) obj;
+        this.obj = (T) obj;
     }
 
     /**
-     *Depending on the passed object type parameter, it calls the necessary inputer
-     * @param pName - type product
+     * Depending on the passed object type parameter, it calls the necessary inputer
+     *
+     * @param productType - type product
      * @return - product
      */
     @Override
-    public T inpProd(String pName, Scanner sc) {
-        Inputer<T> inp = inputerMap.get(pName);
-        T prod = (T) productMap.get(pName);
+    public T inpProd(String productType, Scanner sc) {
+        Inputer<T> inp = inputerMap.get(productType);
+        T prod = (T) productMap.get(productType);
 
         if (!obj.getClass().isAssignableFrom(prod.getClass())) {
             throw new IllegalArgumentException("Product type mismatch");
@@ -35,8 +38,9 @@ public class ConsoleInp<T extends Product> implements Add<T> {
 
     /**
      * Set available products
-     * @param key - product name for access
-     * @param inp - inputer
+     *
+     * @param key     - product name for access
+     * @param inp     - inputer
      * @param product - product class
      */
     public void mapProducts(String key, Inputer<T> inp, Product product) {
@@ -47,7 +51,7 @@ public class ConsoleInp<T extends Product> implements Add<T> {
     public void init() {
         mapProducts("prod", new InputerProducts(), new Product());
         mapProducts("ref", new InputerRefregerators(), new Refregerators());
-        mapProducts("mda", new InputerMda(), new Mda());
+        mapProducts("mda", new InputerMda(), new MediumDigitalAppliances());
     }
 
 }
--- electronics store/src/main/java/com/epam/verizhenko_andrii/electronicsStore/reflectionInputer/Add.java
+++
@@ -1,7 +0,0 @@
-package com.epam.verizhenko_andrii.electronicsStore.reflectionInputer;
-
-import java.util.Scanner;
-
-public interface Add<T> {
-     T inpProd(String pName, Scanner sc);
-}
